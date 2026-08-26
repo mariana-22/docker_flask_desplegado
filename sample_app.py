35d3f75d60cda43323fb4f5c641e8d2376075cb7
mari <valderramakaren09@gmail.com>
2026-08-25 20:07:38 -0500

    refactorizar

--- sample_app.py
+++ sample_app.py
@@ -11,11 +11,11 @@ sample = Flask(__name__)
 def home():
 
     try:
-        # Se leen las credenciales desde variables de entorno
-        db_host = os.getenv("DB_HOST", "servidor-bd-082")
-        db_user = os.getenv("DB_USER", "root")
-        db_password = os.getenv("DB_PASSWORD", "")
-        db_name = os.getenv("DB_NAME", "082_db")
+        # Credenciales obtenidas desde variables de entorno
+        db_host = os.getenv("DB_HOST")
+        db_user = os.getenv("DB_USER")
+        db_password = os.getenv("DB_PASSWORD")
+        db_name = os.getenv("DB_NAME")
 
         conn = pymysql.connect(
             host=db_host,
@@ -26,19 +26,18 @@ def home():
 
         conn.close()
 
-        db_status = "Conexión exitosa a la base de datos prueba para CI, CD para despliegue continuo"
+        db_status = "Conexión exitosa a la base de datos prueba para CI/CD"
 
     except Exception as e:
         db_status = f"Error al conectar a la base de datos: {e}"
 
-    # FALLO INTENCIONAL PARA EL EJERCICIO
-    return f"<h1>Error intencional</h1><p>{db_status}</p>", 500
+    return f"<h1>Bienvenido a la aplicación Flask</h1><p>{db_status}</p>"
 
 
 if __name__ == "__main__":
-    # FALLO INTENCIONAL PARA BANDIT
+    # Modo debug desactivado
     sample.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=5050,
         debug=False
     )
